82f8789fe7ca14de8e72e1485ba859bf45694dd8
nabendu82 <nabendu.biswas@gmail.com>
2020-02-18 14:15:45 +0530

    Part 6 blog complete

---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostCard from './PostCard';
+
+const PostList = () => {
+    return (
+        <>
+            PostList Component
+            <PostCard />
+        </>
+    )
+}
+
+export default PostList;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,49 @@
 import React from "react";
 import Layout from "../components/Layout";
-import styles from "../css/home.module.css"
+import styles from "../css/home.module.css";
+import PostList from "../components/PostList";
+import { graphql, useStaticQuery } from "gatsby";
 
-export default () => (
-    <Layout>
-        <div className={styles.home}>
-            <section className={styles.right__sec}></section>
-            <section className={styles.blog__sec}></section>
-            <section className={styles.left__sec}></section>
-        </div>
-    </Layout>
-)
+const getPosts = graphql`
+{
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        totalCount
+        edges {
+            node {
+            frontmatter {
+                title
+                slug
+                date(formatString: "MMMM Do, YYYY")
+                author
+                image {
+                childImageSharp {
+                    fluid {
+                    ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+                }
+            }
+            excerpt
+            }
+        }
+    }
+}
+`
+
+export default () => {
+    const response = useStaticQuery(getPosts);
+    const posts = response.allMdx.edges;
+    console.log(response);
+
+    return (
+        <Layout>
+            <div className={styles.home}>
+                <section className={styles.right__sec}></section>
+                <section className={styles.blog__sec}>
+                    <PostList post={posts} />
+                </section>
+                <section className={styles.left__sec}></section>
+            </div>
+        </Layout>
+    )
+}
