7f895d7b3770b77eb8132a66c2e7aedb1a6f537f
nabendu82 <nabendu.biswas@gmail.com>
2020-02-18 11:09:01 +0530

    Part 5 blog complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from "react"
+import styles from "../css/footer.module.css"
+import links from "../constants/links"
+import socialIcons from "../constants/social-icons"
+import { Link } from "gatsby"
 
 const Footer = () => {
     return (
-        <h3>
-            Footer Component
-        </h3>
+    <footer className={styles.footer}>
+        <div className={styles.links}>
+        {links.map((item, index) => {
+            return (
+            <Link key={index} to={item.path}>
+                {item.text}
+            </Link>
+            )
+        })}
+        </div>
+        <div className={styles.icons}>
+        {socialIcons.map((item, index) => {
+            return (
+            <a key={index} href={item.url} target="_blank" rel="noopener noreferrer">{item.icon}</a>
+            )
+        })}
+        </div>
+        <div className={styles.copyright}>
+            copyright &copy; The Web Dev, {new Date().getFullYear()} all rights reserved
+        </div>
+    </footer>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout";
+import styles from "../css/home.module.css"
 
 export default () => (
-    <Layout>Hello world!</Layout>
+    <Layout>
+        <div className={styles.home}>
+            <section className={styles.right__sec}></section>
+            <section className={styles.blog__sec}></section>
+            <section className={styles.left__sec}></section>
+        </div>
+    </Layout>
 )
