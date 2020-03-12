a2e6a4e71eb14bc0fa225cf795bb26ece8f7979d
nabendu82 <nabendu.biswas@gmail.com>
2020-03-12 22:26:53 +0530

    Part 21 blog complete

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from "../components/Layout";
+import ContactComp from '../components/Contact/Contact'
 
 const Contact = () => {
     return (
         <Layout>
-            Contact Page
+            <ContactComp />
         </Layout>
     )
 }
