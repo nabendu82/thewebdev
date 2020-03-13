4b2529c0308e76f39bfd8f3590c2c2fdbe1a4272
nabendu82 <nabendu.biswas@gmail.com>
2020-03-13 11:01:22 +0530

    SEO Plugins added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,33 @@
 module.exports = {
+  siteMetadata: {
+    title: `TheWebDev Blog`,
+    author: `Nabendu Biswas`,
+    description: `Blog post by Nabendu Biswas to share stuff i learn in my Web development journey`,
+    siteUrl: `https://thewebdev.tech/`,
+    keywords: [
+      "Nabendu",
+      "Biswas",
+      "UI Lead",
+      "GatsbyJs",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "ReactJs",
+      "React developer",
+      "Front-end Engineer",
+      "GraphQL"
+    ],
+    social: {
+      twitter: `nabendu82`,
+      github: `nabendu82`,
+      email: `nabendu.biswas@gmail.com`,
+    },
+  },
   plugins: [
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-mdx`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -9,6 +35,13 @@ module.exports = {
         path: `src/posts`,
       },
     },
-    `gatsby-plugin-mdx`
+    {
+      resolve: `gatsby-plugin-robots-txt`,
+      options: {
+          host: 'https://thewebdev.tech/',
+          sitemap: 'https://thewebdev.tech/sitemap.xml',
+          policy: [{ userAgent: '*', allow: '/' }]
+      }
+    }
   ]
 }
