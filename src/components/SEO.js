f9598454f17b9beba3bfa8f572eb7c27b1f4d958
nabendu82 <nabendu.biswas@gmail.com>
2020-03-14 09:25:08 +0530

    FB and twitter cards added

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -24,6 +24,18 @@ const SEO = ({ title, description }) => {
         <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
             <meta name="description" content={description || siteDesc} />
             <meta name="image" content={image} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:creator" content={twitterUsername} />
+            <meta name="twitter:title" content={siteTitle} />
+            <meta name="twitter:description" content={siteDesc} />
+            <meta name="twitter:image" content={`${siteUrl}${image}`} />
+            <meta property="og:url" content={siteUrl} />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={siteTitle} />
+            <meta property="og:description" content={siteDesc} />
+            <meta property="og:image" content={`${siteUrl}${image}`} />
+            <meta property="og:image:width" content="400" />
+            <meta property="og:image:height" content="300" />
         </Helmet>
     )
 }
