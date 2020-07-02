d66b25915755e6a9637e962ff77c7d766ffbd268
Nabendu Biswas <nabendubiswas@Nabendus-MacBook-Air.local>
2020-07-02 11:00:46 +0530

    Link issue fixed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ const AboutComp = () => {
             <div className={styles.center}>
                 <h3>About Us</h3>
                 <p><i>The Web Dev(TWD)</i> is a place, where you will find all the resources to take your
-                    Web-development to the next level. It contains full length blog series, with detailed
+                    Web-development to the next level. It contains full length blog series, youtube videos with detailed
                     instruction to build production level web-apps.
                 </p>
                 <p>We currently have blog series to build web-apps in React, GraphQL and GatsbyJS. Beside
@@ -16,10 +16,6 @@ const AboutComp = () => {
                 <p>This site is developed and maintained by Nabendu Biswas. He is a UI Lead at Innominds
                     and a passionate tech blogger. You can read more about him on his personal
                     site <a href="https://nabendu.me/" target="_blank" rel="noopener noreferrer">nabendu.me</a>
-                </p>
-                <p>This site is currently in a transition mode, with blogs transferred everyday. You
-                    can read all our blogs currently
-                    at <a href="https://nabendu.blog/" target="_blank" rel="noopener noreferrer">nabendu.blog</a>
                 </p>
             </div>
         </section>
